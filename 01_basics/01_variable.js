b62773ef3d3bfc33eca2fc060358a03d4fc964c4
pushpak076 <144616856+pushpak076@users.noreply.github.com>
2023-10-10 09:45:58 +0000

    control confige change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+//lets learn about variable.
+
+const accounId=1125456;
+let accounEmail="javaScript@gmail.com"; // most used in scope
+var accountPasssword="12345";
+accountCity="jaipur";
+let accounState;
+
+// accounId=2;   const variable can not change.
+console.log(accounId);
+
+accounEmail="Nodejs@gmail.com";
+console.table([accounId , accounEmail, accountPasssword , accountCity,accounState]);
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat aal js code as never version
+ 
+
+// alert(1+2); // we are using node js, not browser
+
+//document on [mdn and  tc39.es]
+
+let name="string";
+let age=18;
+let isloggedIn=false;
+
+//number = 2^53
+//bigInt for big data
+// string="str"
+//boolean =true/false
+// null=standard value (can be object);
+// undefined=not assign
+// symbol ->to find uniqeness  most in react
+
+// object
+
+console.log(typeof null);
