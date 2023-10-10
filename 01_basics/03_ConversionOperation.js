207736ff8c64f6370a960a295b0e7e1e86057c7a
pushpak076 <144616856+pushpak076@users.noreply.github.com>
2023-10-10 17:17:51 +0000

    conversion of datatype

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,11 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score);
+console.log(typeof score);
+
+let scr="33abc";
+let valueInNumber2=Number(scr);
+console.log(valueInNumber2);
