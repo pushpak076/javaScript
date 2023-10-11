d29790fffc9b4d1d8bdb574dabafca9303c10463
pushpak076 <144616856+pushpak076@users.noreply.github.com>
2023-10-11 11:36:51 +0000

    date function discuss

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,26 @@
+// javaScript date objet represent a single moment in time in a platform independent format.
+// date begining of jan 1,1970,UTC(the epoch).
+
+let myDate=new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());  //most important 
+
+let myCreatedDate=new Date(2000,3,20);
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStemp=Date.now();
+// console.log(myTimeStemp.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+newDate.toLocaleDateString('default',{
+    weekday: "long",
+})
