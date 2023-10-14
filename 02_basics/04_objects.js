ae354e8b7c0a5d0f844e6f9f4c1094bf24b3540b
pushpak076 <144616856+pushpak076@users.noreply.github.com>
2023-10-14 13:05:34 +0000

    object singelton

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,81 @@
+// //singelton or constructor
+
+
+// const tinderUser=new Object()
+
+// tinderUser.id="1232as";
+// tinderUser.name="harish";
+
+// // console.log(tinderUser);
+
+// const regularUser={
+//     email:"somesh@gmail.com",
+//     fullname:{
+//         username:{
+//             firstName:"hitesh",
+//             lastNmae:"patel"
+//         }
+//     }
+// }
+// // console.log(regularUser);
+
+// // const obj1={1:"a",2:"b"};
+// // const obj2={3:"c",4:"d"};
+
+// // const obj3={obj1 , obj2};
+// // console.log(obj3);
+
+// // const assign_obj=Object.assign(obj1,obj2);
+// // console.log(assign_obj);
+
+// // const obj={...obj1, ...obj2};
+// // console.log(obj);
+
+
+// const users=[
+//     {
+//         id:1,
+//         email:  "one@gmail.com"
+//     },
+//     {
+//         id:2,
+//         email : "two@gmail.com"
+//     },
+//     {
+//         id:3,
+//         email : "three@gmail.com"
+//     }
+// ]
+
+// // console.log(users);
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('id'));
+
+
+// object destructuring
+
+// const course={
+//     courseName:"java Script",
+//     price:"free",
+//     teacher:"self"
+// }
+
+// const{teacher}=course;
+// const{price:pr}=course;
+// console.log(teacher);
+// console.log(pr);
+
+
+//json 
+
+// {
+//     "courseName":"java Script",
+//     "price":"free",
+//     "teacher":"self"
+// }
